41de89dfab79fcf514572f58a5aebc323cd0211c
unknown <cd232310@gmail.com>
2019-05-05 22:46:36 +0800

    使用森林

--- Hw1/Hw.py
+++ Hw1/Hw.py
@@ -1,8 +1,16 @@
 import pandas as file
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+forest_model = RandomForestClassifier(criterion='gini',
+                            n_estimators=1000,
+                            min_samples_split=12,
+                            min_samples_leaf=1,
+                            oob_score=True,
+                            random_state=1,
+                            n_jobs=-1)
 
 data = file.read_csv('./train.csv')
-model = DecisionTreeRegressor(random_state=1)
 
 # Translate data type
 data['Attribute16'] = data['Attribute16'].map({'New_Visitor': 0, 'Returning_Visitor': 1, 'Other': 2})
@@ -15,7 +23,7 @@ res = data['Attribute18']
 # Deleta not using data
 data = data.drop(['Attribute1'],axis=1)
 data = data.drop(['Attribute2'],axis=1)
-data = data.drop(['Attribute11'],axis=1)
+data['Attribute11'] = data['Attribute11'].map({'Jan': 0, 'Feb': 1,'Mar': 2,'Apr': 3,'May': 4,'June': 5,'Jul': 6,'Aug': 7,'Sep': 8,'Oct': 9,'Nov': 10,'Dec': 11})
 data = data.drop(['Attribute12'],axis=1)
 data = data.drop(['Attribute13'],axis=1)
 data = data.drop(['Attribute14'],axis=1)
@@ -23,7 +31,8 @@ data = data.drop(['Attribute15'],axis=1)
 data = data.drop(['Attribute18'],axis=1)
 
 # Train model
-model.fit(data,res)
+train_X, val_X, train_y, val_y = train_test_split(data, res, random_state = 1)
+forest_model.fit(train_X,train_y)
 
 test = file.read_csv('./test.csv')
 test['Attribute16'] = test['Attribute16'].map({'New_Visitor': 0, 'Returning_Visitor': 1, 'Other': 2})
@@ -32,17 +41,17 @@ test['Attribute17'] = test['Attribute17'].map({False: 0, True: 1})
 # Deleta not using data
 test = test.drop(['Attribute1'],axis=1)
 test = test.drop(['Attribute2'],axis=1)
-test = test.drop(['Attribute11'],axis=1)
+test['Attribute11'] = test['Attribute11'].map({'Jan': 0, 'Feb': 1,'Mar': 2,'Apr': 3,'May': 4,'June': 5,'Jul': 6,'Aug': 7,'Sep': 8,'Oct': 9,'Nov': 10,'Dec': 11})
 test = test.drop(['Attribute12'],axis=1)
 test = test.drop(['Attribute13'],axis=1)
 test = test.drop(['Attribute14'],axis=1)
 test = test.drop(['Attribute15'],axis=1)
 
-predict = model.predict(test)
+#predict = model.predict(test)
+predict = forest_model.predict(test)
 ans = []
 for row in predict:
     ans.append(row)
-    
 # Output Ans
 with open('output.csv', 'w') as f:
     f.write("id,ans\n")
